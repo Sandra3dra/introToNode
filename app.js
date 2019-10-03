fe6f1e6c2669f041d6fcd116fd46fa043a8a5f16
Sandra Tsao <sandratsao@Sandras-MBP.fc.ca>
2019-10-03 13:51:03 -0400

    express js first

--- app.js
+++ app.js
@@ -1,14 +1,57 @@
-const http = require('http');
+// const http = require('http');
 
-const hostname = '127.0.0.1'; //this is local host
-const port = process.env.PORT || 3000; //this is a node convention, change to anything
+// const hostname = '127.0.0.1'; //this is local host
+// const port = process.env.PORT || 3000; //this is a node convention, change to anything
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
-});
+// const server = http.createServer((req, res) => {
+//   res.statusCode = 200;
+//   res.setHeader('Content-Type', 'text/plain');
+//   res.end('Hello World\n');
+// });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+const express = require("express");
+const hbs = require("hbs");
+const path = require("path");
+const sql = require("./utils/sql");
+
+const port = process.env.PORT || 3000;
+const app = express();
+app.use(express.static("public"));
+
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname + "/views"));
+
+app.get("/", (req, res) => {
+    res.render("home", {homemessage: "hey there", bio: "some bios here and there"});
+})
+
+app.get("/users", (req, res) => {
+  //get user data when hit this
+
+  //try a connection
+  //if error happens
+  sql.getConnection((err, connection) => {
+    if (err) {
+      return console.log(err,message);
+    }
+
+    let query = "SELECT * FROM tbl_card";
+
+    sql.query(query, (err, rows) => {
+      //done with db connect, let someone else use it
+      connection.release(); 
+
+      if (err) {return console.log(err.message)}
+
+      console.log(rows);
+
+      res.render("user", rows[0]);
+    })
+
+
+  })
+})
+
+app.listen(port, () => {
+  console.log(`app is running on port ${port}`);
+})
---
+++ config.js
@@ -0,0 +1,11 @@
+// import { builtinModules } from "module"
+
+const config = {
+    uname: "root",
+    upass: "root", //windows leave this blank
+    port: 8889,  //3306 for win and linux
+    host: "localhost",
+    db: "bizcard"
+}
+
+module.exports = config;
